2a8112bd2d9f51b86767f1a1ff92c0c0e15a87be
高橋　宗太 <souta_takahashi@012grp.co.jp>
2021-05-18 10:49:13 +0900

    no message

--- next.config.js
+++ next.config.js
@@ -1,11 +1,12 @@
 module.exports = {
   trailingSlash: true,
+  assetPrefix: '/dynamic_import_practice',
   webpack: (config, { webpack }) => {
     config.plugins.push(
       new webpack.ProvidePlugin({
-        React: 'react'
+        React: 'react',
       })
     )
     return config
-  }
+  },
 }
